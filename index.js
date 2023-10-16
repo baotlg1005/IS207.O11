8407ccf8095edda6210407a0ae25fe5f2c158a7d
baotlg1005 <baotlg1005@gmail.com>
2023-10-16 07:56:16 +0700

    on scroll change header done

---
+++ index.js
@@ -0,0 +1,10 @@
+window.onscroll = function() {scrollFunction()};
+
+//scroll function: change header background color and text color on scroll
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("header").classList.add("on-scroll");
+  } else {
+    document.getElementById("header").classList.remove("on-scroll");
+  }
+}
