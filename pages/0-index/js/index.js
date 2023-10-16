755f945d5a925679a208850576512522ebfc4b70
baotlg1005 <baotlg1005@gmail.com>
2023-10-16 23:49:26 +0700

    add event for feature btn

--- pages/0-index/js/index.js
+++ pages/0-index/js/index.js
@@ -8,3 +8,21 @@ function scrollFunction() {
     document.getElementById("header").classList.remove("on-scroll");
   }
 }
+
+//selected feature 
+const featureBtns = document.querySelectorAll('.feature-container__navbar .navbar__item');
+const featureContainers = document.querySelectorAll('.feature-container__item');
+console.log(featureBtns);
+featureBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    featureBtns.forEach(btn => btn.classList.remove('selected'));
+    btn.classList.add('selected');
+    const target = btn.dataset.featureType;
+    featureContainers.forEach(feature => {
+      feature.classList.remove('active');
+      if (feature.dataset.featureType === target) {
+        feature.classList.add('active');
+      }
+    });
+  });
+});
