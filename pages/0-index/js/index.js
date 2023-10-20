7783fb1c8f44c9ee5bf280336715d512d1da0ba7
baotlg1005 <baotlg1005@gmail.com>
2023-10-20 23:42:53 +0700

    plane -> flight; complete flight feature interface

--- pages/0-index/js/index.js
+++ pages/0-index/js/index.js
@@ -30,18 +30,55 @@ featureButtons.forEach(btn => {
 
 
 //feature-container
-//--plane--
-const planeFeatureContainer = document.querySelector('.feature-container__plane');
-const tripTypeToggle = {
-  toggleButton: planeFeatureContainer.querySelector('.plane__trip-type-toggle'),
-  toggleIndex: 0,
+//--flight--
+const flightFeatureContainer = document.querySelector('.feature-container__flight');
+const flightTypeToggle = {
+  toggleButtons: flightFeatureContainer.querySelectorAll('.flight__flight-type-toggle .btn-toggle'),
+  currentToggleIndex: 0,
 }
-const selectPassengerBtn = planeFeatureContainer.querySelector('.plane__select-passenger-btn');
-const filterSeatBtn = planeFeatureContainer.querySelector('.plane__filter-seat-btn');
+const selectPassengerBtn = flightFeatureContainer.querySelector('.flight__select-passenger-btn');
+const filterSeatBtn = flightFeatureContainer.querySelector('.flight__filter-seat-btn');
 
+const oneFlightElement = [
+  flightFeatureContainer.querySelector('.flight-info-item--one-flight'),
+];
+const manyFlightElement = [
+  flightFeatureContainer.querySelector('.flight__many-flight-btn-group'),
+  flightFeatureContainer.querySelector('.flight-info-item--many-flight'),
+];
 
+// toggle flight type 
+// -> if one flight -> show one flight element, hide many flight element 
+// -> if many flight -> show many flight element, hide one flight element
+flightTypeToggle.toggleButtons.forEach(btn => {
+  btn.addEventListener('click', function(e) {
+    const flightType = this.dataset.flightType;
+    flightTypeToggle.currentToggleIndex = flightType === 'one' ? 0 : 1;
+    flightTypeToggle.toggleButtons.forEach(btn => btn.classList.remove('on'));
+    this.classList.add('on');
 
+    if(flightType === 'one') {
+      oneFlightElement.forEach(el => el.classList.remove('hide'));
+      manyFlightElement.forEach(el => el.classList.add('hide'));
+    }
+    else {
+      oneFlightElement.forEach(el => el.classList.add('hide'));
+      manyFlightElement.forEach(el => el.classList.remove('hide'));
+    }
+  })
+});
 
-
-
+// checkbox have-return-flight
+const haveReturnFlightCheckbox = document.getElementById("flight-return-checkbox");
+const returnDateSelectDropdown = flightFeatureContainer.querySelector('.return-date__select-dropdown');
+// add a event listener to checkbox
+// -> if checked -> enable return date select dropdown else disable
+haveReturnFlightCheckbox.addEventListener('change', function(e) {
+  if(this.checked) {
+    returnDateSelectDropdown.classList.remove('disable');
+  }
+  else {
+    returnDateSelectDropdown.classList.add('disable');
+  }
+});
 
