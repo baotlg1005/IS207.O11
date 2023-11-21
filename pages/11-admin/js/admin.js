1dc51fccb6924bf13fc9bcd95d92e900c05388a6
baotlg1005 <baotlg1005@gmail.com>
2023-11-21 23:30:58 +0700

    delete admin page client

--- pages/11-admin/js/admin.js
+++
@@ -1,9 +0,0 @@
-new DataTable('#user-table');
-new DataTable('#flight-table');
-new DataTable('#car-table');
-new DataTable('#bus-table');
-new DataTable('#hotel-table');
-new DataTable('#flightbill-table');
-new DataTable('#carbill-table');
-new DataTable('#busbill-table');
-new DataTable('#hotelbill-table');
