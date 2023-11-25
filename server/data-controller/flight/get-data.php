855542d75b01fad95f409804b8f3ee98d02092d3
Tu Tran Le <tranletu2003@gmail.com>
2023-11-25 21:27:47 +0700

    update-css

--- server/data-controller/flight/get-data.php
+++ server/data-controller/flight/get-data.php
@@ -18,7 +18,7 @@ if ($action == "showresult") {
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            $data = $result->fetch_all(MYSQLI_ASSOC);
+            $data = $result->fetch_all(MYSQLI_ASSOC);   
             echo json_encode($data, JSON_UNESCAPED_UNICODE);
         }
     } else {
