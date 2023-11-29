b580bfd288ae63f8238c0a18b94620df2f67cd96
Tu Tran Le <tranletu2003@gmail.com>
2023-11-29 23:57:37 +0700

    finish-get-data

--- pages/payment-flight/js/payment-flight.js
+++ pages/payment-flight/js/payment-flight.js
@@ -9,59 +9,58 @@ window.onload = function () {
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             let flightInfo = JSON.parse(this.responseText);
-            if (flightInfo.length > 0) {
+            if (flightInfo) {
                 document.getElementById("payment-window__flight-info").innerHTML = "";
                 document.getElementById("payment-window__price-info").innerHTML = "";
-                for (let i = 0; i < flightInfo.length; i++) {
-                    document.getElementById("payment-window__flight-info").innerHTML += `
+                document.getElementById("payment-window__flight-info").innerHTML += `
                     <div class="container row-container flex-row justify-between">
                     <div class="content-container flex-row align-center">
                         <div class="label">Điểm đi:</div>
-                        <div class="text" id="txt-from">${flightInfo[i].From}</div>
+                        <div class="text" id="txt-from">${flightInfo.From}</div>
                     </div>
                     <div class="content-container flex-row align-center">
                         <div class="label">Điểm đến:</div>
-                        <div class="text" id="txt-to">${flightInfo[i].To}</div>
+                        <div class="text" id="txt-to">${flightInfo.To}</div>
                     </div>
                 </div>
                 <div class="container row-container flex-row justify-between">
                     <div class="content-container flex-row align-center">
                         <div class="label">Ngày khởi hành:</div>
-                        <div class="text" id="txt-departureDate">${changeDateFormat(flightInfo[i].Date)}</div>
+                        <div class="text" id="txt-departureDate">${changeDateFormat(flightInfo.Date)}</div>
                     </div>
                     <div class="content-container flex-row align-center">
                         <div class="label">Giờ khởi hành:</div>
-                        <div class="text" id="txt-departureTime">${flightInfo[i].DepartureTime}</div>
+                        <div class="text" id="txt-departureTime">${flightInfo.DepartureTime}</div>
                     </div>
                 </div>
                 <div class="container row-container flex-row justify-between">
                     <div class="content-container flex-row align-center">
                         <div class="label">Thời gian bay:</div>
-                        <div class="text" id="txt-travelTime">${flightInfo[i].TravelTime}</div>
+                        <div class="text" id="txt-travelTime">${flightInfo.TravelTime}</div>
                     </div>
                     <div class="content-container flex-row align-center">
                         <div class="label">Giờ hạ cánh:</div>
-                        <div class="text" id="txt-arrivalTime">${flightInfo[i].ArrivalTime}</div>
+                        <div class="text" id="txt-arrivalTime">${flightInfo.ArrivalTime}</div>
                     </div>
                 </div>
                 <div class="container row-container flex-row justify-between">
                     <div class="content-container flex-row align-center">
                         <div class="label">Tên hãng:</div>
-                        <div class="text" id="txt-brand">${flightInfo[i].Name}</div>
+                        <div class="text" id="txt-brand">${flightInfo.Name}</div>
                     </div>
                     <div class="content-container flex-row align-center">
                         <div class="label">Hạng ghế:</div>
-                        <div class="text" id="txt-seatType">${flightInfo[i].SeatClass}</div>
+                        <div class="text" id="txt-seatType">${flightInfo.SeatClass}</div>
                     </div>
                 </div>`
-                    document.getElementById("payment-window__price-info").innerHTML += `
+                document.getElementById("payment-window__price-info").innerHTML += `
                     <div class="container row-container flex-row justify-between">
                     <div class="content-container flex-row align-center">
                         <div class="label">Số vé:</div>
                         <div class="text" id="txt-ticket">x ${flightPaymentInfo.ticketNumber}</div>
                     </div>
                     <div class="content-container flex-row align-center">
-                        <div class="label fs-16" id="txt-price">${changeMoneyFormat(flightInfo[i].Price)} VND</div>
+                        <div class="label fs-16" id="txt-price">${changeMoneyFormat(flightInfo.Price)} VND</div>
                     </div>
                 </div>
                 <div class="container row-container flex-row justify-between">
@@ -69,10 +68,9 @@ window.onload = function () {
                         <div class="label">Tổng giá tiền</div>
                     </div>
                     <div class="content-container flex-row align-center">
-                        <div class="label" id="txt-totalPrice">${changeMoneyFormat(flightInfo[i].Price * flightPaymentInfo.ticketNumber)} VND</div>
+                        <div class="label" id="txt-totalPrice">${changeMoneyFormat(flightInfo.Price * flightPaymentInfo.ticketNumber)} VND</div>
                     </div>
                 </div>`
-                }
             }
             else {
                 document.getElementById("payment-window__flight-info").innerHTML += "ERROR";
@@ -92,11 +90,11 @@ function changeDateFormat(date) {
     let month = dateArray[1];
     let year = dateArray[0];
     let monthArray = ['tháng 1', 'tháng 2', 'tháng 3', 'tháng 4',
-      'tháng 5', 'tháng 6', 'tháng 7', 'tháng 8',
-      'tháng 9', 'tháng 10', 'tháng 11', 'tháng 12'];
+        'tháng 5', 'tháng 6', 'tháng 7', 'tháng 8',
+        'tháng 9', 'tháng 10', 'tháng 11', 'tháng 12'];
     return `${day}, ${monthArray[month - 1]}, ${year}`;
-  }
+}
 
-  function changeMoneyFormat(money) {
+function changeMoneyFormat(money) {
     return money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-  }
+}
